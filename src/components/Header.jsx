c6f7155ab7c39d2d4318521182db2d7142821425
SapphireBloo <156019660+SapphireBloo@users.noreply.github.com>
2025-06-02 13:53:16 -0500

    Fix: Missing link import in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 import "./Header.css";
 
 export default function Header({ onHomeClick }) {
